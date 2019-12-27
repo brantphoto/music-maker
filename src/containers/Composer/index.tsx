f034ffbf10ac327d069019b53413d9d271b8610e
Brant Barger <brant.barger@gmail.com>
2019-12-27 13:56:49 -0800

    made keyboard css dynamic and made keyboard jsx elements an array

--- src/containers/Composer/index.tsx
+++ src/containers/Composer/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import * as Redux from 'redux'
 import {RootState} from '../../rootReducer';
 import {connect} from 'react-redux'
+import classnames from 'classnames'
 import {increment, decrement, autoIncrement} from './reducer' 
 
 type MyProps = { 
@@ -17,6 +18,7 @@ type MyState = {
 class ComposerContainer extends React.Component<MyProps, MyState> {
   render() {
     const {count, increment, decrement, autoIncrement} = this.props;
+    const notes = ['f', 'f-sharp', 'g', 'g-sharp', 'a', 'a-sharp', 'b', 'c', 'c-sharp', 'd', 'd-sharp', 'e']
     return (
       <div>
         <div>
@@ -26,18 +28,19 @@ class ComposerContainer extends React.Component<MyProps, MyState> {
           <button onClick={autoIncrement}>AUTO</button>
         </div>
         <ul className="set">
-          <li className="white b"></li>
-          <li className="black as"></li>
-          <li className="white a"></li>
-          <li className="black gs"></li>
-          <li className="white g"></li>
-          <li className="black fs"></li>
-          <li className="white f"></li>
-          <li className="white e"></li>
-          <li className="black ds"></li>
-          <li className="white d"></li>
-          <li className="black cs"></li>
-          <li className="white c"></li>
+          {
+            notes.map((note) => (
+              <li className={
+                classnames(
+                {
+                white: note.length === 1,
+                black: note.length !== 1,
+                [note]: true,
+                }
+              )
+              }></li> 
+            ))
+          }
         </ul>
       </div>
     );
